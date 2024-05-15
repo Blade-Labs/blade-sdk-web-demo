7bf2fe8700da3d46abf672d1e2eac9fc0e12880b
Denys Shuliar <denshullyar@gmail.com>
2024-05-15 11:29:42 +0300

    removed files

--- src/demoConfig.ts
+++
@@ -1,8 +0,0 @@
-import { SdkEnvironment } from "@bladelabs/blade-sdk.js";
-
-export const demoConfig = {
-    apiKey: 'FG9dUBQcBaBAPgCHz7DqmNZzrJyhewAMJytjwp3VFIEMFTXQyVSIDq6wRvtPcSAt',
-    dAppCode: 'unitysdktest',
-    network: 'Testnet',
-    sdkEnvironment: SdkEnvironment.CI
-}
